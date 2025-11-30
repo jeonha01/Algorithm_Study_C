8f72448eeb9d78f670199bae04a6582ce468f416
jeonha01 <kangjeonha@gmail.com>
2025-11-30 23:42:48 +0900

    refactor: 숫자 맞추기 (사용자가 맞추기) -> 코드 수정

--- school-assignments/assignments11/assignments11.c
+++ school-assignments/assignments11/assignments11.c
@@ -1,43 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define MAX 1000
-int array[MAX];
-int count = 0;
 
 int main()
 {
-    srand(time(NULL));
-
-    int ComRan = rand() % 1000 + 1;
-
-    for (int i = 0; i < MAX; i++)
-    {
-        array[i] = i + 1;
-    }
+    srand((unsigned)time(NULL));      
+    int ComRan = rand() % 1000 + 1;  
+    int count = 0;
+    int ans;
 
     while (1)
     {
-        int ans;
         scanf("%d", &ans);
         count++;
 
         if (ans == ComRan)
         {
+            printf("%d번 만에 성공\n", count);
             break;
         }
-        else if (array[ans + 1] > ComRan)
+        else if (ans > ComRan)
         {
             printf("다운\n");
-            continue;
         }
         else
         {
             printf("업\n");
-            continue;
         }
     }
-
-    printf("%d번 만에 성공", count);
 }
